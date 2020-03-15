d1c40fb4af44937ff465ede09ee78d66f8a9689c
ConanShin <conan.shin@sk.com>
2020-03-15 19:25:29 +0900

    sort by date

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ app.use(cors())
 let response = []
 
 const doosanOnly = items => {
-    return items.filter(item => item["아파트"] === '두산')
+    return items.filter(item => item["아파트"] === '두산').sort((a, b) => b["년"] - a["년"] || b["월"] - a["월"] || b["일"] - a["일"])
 }
 
 app.get('/', async (req, res) => {
@@ -20,6 +20,8 @@ app.get('/', async (req, res) => {
     LAST_MONTH.setMonth(LAST_MONTH.getMonth() - 1)
     LAST_MONTH = '' + LAST_MONTH.getFullYear() + ('0' + (LAST_MONTH.getMonth() + 1)).slice(-2)
     THIS_MONTH = '' + THIS_MONTH.getFullYear() + ('0' + (THIS_MONTH.getMonth() + 1)).slice(-2)
+    THIS_MONTH = '202001'
+    LAST_MONTH = '201912'
     const URL_LAST_MONTH = `http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=${LAWD_CD}&DEAL_YMD=${LAST_MONTH}&ServiceKey=${ServiceKey}`
     const URL_THIS_MONTH = `http://openapi.molit.go.kr:8081/OpenAPI_ToolInstallPackage/service/rest/RTMSOBJSvc/getRTMSDataSvcAptTrade?LAWD_CD=${LAWD_CD}&DEAL_YMD=${THIS_MONTH}&ServiceKey=${ServiceKey}`
     try {
